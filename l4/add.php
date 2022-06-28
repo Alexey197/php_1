6a733bfa841a52eab9705644a4e2f5dfb27dd0ab
Alexey197 <margadonnn3@gmail.com>
2022-06-28 19:36:14 +0300

    error

--- l4/add.php
+++ l4/add.php
@@ -7,11 +7,26 @@
     $sql = "INSERT messages (name, text, status) VALUES (:name, :text, :status)";
     $query = $db->prepare($sql);
 
-    $name = 'Admin';
-    $text = 'Delete all!';
-    $status = 1;
+    $params = [
+        'name' => 'Admin',
+        'text' => 'Delete all!',
+        'status' => 1
+    ];
 
-    $query->bindParam(':name', $name);
-    $query->bindParam(':text', $text);
-    $query->bindParam(':status', $status);
-    $query->execute();
+    $query->execute($params);
+    $errInfo = $query->errorInfo();
+    if($errInfo[0] !== PDO::ERR_NONE){
+        echo $errInfo[2];
+        exit();
+    }
+
+    echo 'All done!';
+
+//    $name = 'Admin';
+//    $text = 'Delete all!';
+//    $status = 1;
+//
+//    $query->bindParam(':name', $name);
+//    $query->bindParam(':text', $text);
+//    $query->bindParam(':status', $status);
+//    $query->execute();
