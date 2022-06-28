647105bb32d56da8b06d9f1a95392b8ea614c5c9
Alexey197 <margadonnn3@gmail.com>
2022-06-28 14:53:31 +0300

    bindParam

--- l4/add.php
+++ l4/add.php
@@ -2,5 +2,16 @@
 
     $db = new PDO('mysql:host=localhost;dbname=php1simple', 'root', 'root');
     $db->exec('SET NAMES UTF8');
-    $sql = "INSERT messages (name, text) VALUES (:u, :text)";
-    $db->exec($sql);
+
+
+    $sql = "INSERT messages (name, text, status) VALUES (:name, :text, :status)";
+    $query = $db->prepare($sql);
+
+    $name = 'Admin';
+    $text = 'Delete all!';
+    $status = 1;
+
+    $query->bindParam(':name', $name);
+    $query->bindParam(':text', $text);
+    $query->bindParam(':status', $status);
+    $query->execute();
