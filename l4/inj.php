186c3f791419a0d44637910ed5a401050f04b5f1
Alexey197 <margadonnn3@gmail.com>
2022-06-28 13:51:33 +0300

    quote() method

--- l4/inj.php
+++ l4/inj.php
@@ -3,9 +3,13 @@
 $db = new PDO('mysql:host=localhost;dbname=php1simple', 'root', 'root');
 $db->exec('SET NAMES UTF8');
 
-$text = 'Hello All 333!'; //$_POST['text'];
-$id = $_GET['id'];
 
-$sql = "UPDATE messages SET text='$text' WHERE id_message=$id";
+//?id=2&text=HelloAdmin',status='1
+$text = $db->quote($_GET['text']);
+var_dump($_GET['text']);
+var_dump($text);
+$id = (int)($_GET['id'] ?? '');
+
+$sql = "UPDATE messages SET text=$text WHERE id_message=$id";
 $db->exec($sql);
 
