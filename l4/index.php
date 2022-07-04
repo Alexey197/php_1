79fb4921c0ffcec8fb3551441c896137b710014a
Alexey197 <margadonnn3@gmail.com>
2022-07-04 18:43:46 +0300

    starting ht-l4

--- l4/index.php
+++ l4/index.php
@@ -1,22 +1,21 @@
 <?php
 
 include_once('model/messages.php');
-
 $messages = messagesAll();
 
 ?>
-<div>
     <h1>Chat</h1>
     <a href="add.php">add</a>
-    <? foreach ($messages as $message): ?>
     <div>
-        <strong><?=$message['name']?></strong>
-        <em><?=$message['dt_add']?></em>
-        <div>
-            <?=$message['text']?>
-        </div>
-      <hr>
+        <? foreach ($messages as $message): ?>
+          <div>
+            <strong><?=$message['name']?></strong>
+            <em><?=$message['dt_add']?></em>
+            <div>
+                <?=$message['text']?>
+              <a href="message.php?id=$message['id_message']">Read more</a>
+            </div>
+            <hr>
+          </div>
+        <? endforeach; ?>
     </div>
-    <? endforeach; ?>
-
-</div>
---
+++ l4/message.php
@@ -0,0 +1,12 @@
+<?php
+
+include_once('model/messages.php');
+$id = $_GET['id'] ?? '';
+
+?>
+    <h1>Chat</h1>
+    <a href="add.php">add</a>
+    <div>
+      
+    </div>
+      
