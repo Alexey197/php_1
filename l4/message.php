660567679a9dda6e9c09c5148a42e22422f7e68c
Alexey197 <margadonnn3@gmail.com>
2022-07-05 15:06:51 +0300

    message.php

--- l4/message.php
+++ l4/message.php
@@ -1,12 +1,26 @@
 <?php
 
 include_once('model/messages.php');
-$id = $_GET['id'] ?? '';
+$strId = $_GET['id'] ?? '';
+
+$id = (int)$strId;
+$message = messagesOne($id);
+$hasMsg = $message !== false;
 
 ?>
-    <h1>Chat</h1>
-    <a href="add.php">add</a>
-    <div>
-      
-    </div>
-      
+
+<div>
+  <h1>Chat</h1>
+  <a href="index.php">index</a>
+
+    <? if ($hasMsg): ?>
+      <div>
+        <strong><?=$message['name']?></strong>
+        <em><?=$message['dt_add']?></em>
+        <div><?=$message['text']?></div>
+      </div>
+
+    <? else: ?>
+      <p>Article not found</p>
+    <? endif; ?>
+</div>
--- l4/model/messages.php
+++ l4/model/messages.php
@@ -13,3 +13,19 @@ function messagesAdd(array $fields) : bool {
     dbQuery($sql, $fields);
     return true;
 }
+
+//declare(strict_types=1)
+/*
+function messagesOne(int $id) : ?array{
+    $sql = "SELECT * FROM messages WHERE id_message=:id";
+    $query = dbQuery($sql, ['id' => $id]);
+    $article = $query->fetch();
+    return is_array($article) ? $article : null;
+}
+*/
+
+function messagesOne(int $id){
+    $sql = "SELECT * FROM messages WHERE id_message=:id";
+    $query = dbQuery($sql, ['id' => $id]);
+    return $query->fetch();
+}
